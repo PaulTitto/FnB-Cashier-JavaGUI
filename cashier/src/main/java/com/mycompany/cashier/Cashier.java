b505d826d441f6697e57003bef8164ae9708b9df
PaulTitto <paulustito555@gmail.com>
2023-11-28 06:01:16 +0700

    Adding File

---
+++ cashier/src/main/java/com/mycompany/cashier/Cashier.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cashier;
+
+/**
+ *
+ * @author PaulTitto
+ */
+public class Cashier {
+
+    public static void main(String[] args) {
+        new Login().show();
+    }
+}
